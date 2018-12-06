9018c4308413fcc177cb77643e0937094984a30e
Srujan19997 <44336588+Srujan19997@users.noreply.github.com>
2018-12-06 19:01:02 +0530

    Add files via upload

---
+++ FizzBuzz.ps1
@@ -0,0 +1,18 @@
+﻿For($x = 1; $x -le 100; $x++) {
+  $Threes = $x % 3
+  $Fives = $x % 5
+
+  If (($Threes -eq 0) -and ($Fives -eq 0)) 
+  {
+    Write-Host "FizzBuzz"
+  }
+   Elseif ($Threes -eq 0) {
+    Write-Host "Fizz"
+  } 
+  Elseif ($Fives -eq 0) {
+    Write-Host "Buzz"
+  } 
+  Else {
+    Write-Host $x
+  }
+}
