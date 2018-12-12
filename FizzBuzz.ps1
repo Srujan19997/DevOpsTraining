29c4a46fedcde748200c5023e32e8df069fab2d6
Srujan19997 <44336588+Srujan19997@users.noreply.github.com>
2018-12-12 14:23:20 +0530

    Update FizzBuzz.ps1

--- FizzBuzz.ps1
+++ FizzBuzz.ps1
@@ -1,18 +1,28 @@
-﻿For($x = 1; $x -le 100; $x++) {
+ [int]$StartNumber = Read-Host  'Enter the start number'
+[int]$StopNumber = Read-Host 'Enter the stop number'
+[int]$ElementsInLine=  Read-Host  'Enter number of elements to print in one line'
+[int]$temp=0;
+For($x = $StartNumber ; $x -le $StopNumber ; $x++,$temp++) {
   $Threes = $x % 3
   $Fives = $x % 5
+  if($temp -eq $ElementsInLine)
+  {
+  $temp=0  
+  Write-Host " "
+  
+  }
 
   If (($Threes -eq 0) -and ($Fives -eq 0)) 
   {
-    Write-Host "FizzBuzz"
+    Write-Host   "  FizzBuzz($x)" -NoNewline
   }
    Elseif ($Threes -eq 0) {
-    Write-Host "Fizz"
+    Write-Host  "  Fizz($x)  " -NoNewline 
   } 
   Elseif ($Fives -eq 0) {
-    Write-Host "Buzz"
+    Write-Host  "  Buzz($x)  " -NoNewline 
   } 
   Else {
-    Write-Host $x
+    Write-Host  " "$x -NoNewline
   }
 }
