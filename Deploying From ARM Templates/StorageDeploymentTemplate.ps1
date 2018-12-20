724912f02588a658d5a91f521a2f4e78e64a75df
Srujan19997 <44336588+Srujan19997@users.noreply.github.com>
2018-12-20 11:50:49 +0530

    Add files via upload

---
+++ Deploying From ARM Templates/StorageDeploymentTemplate.ps1
@@ -0,0 +1,13 @@
+//Deploying a  storage account using ARM templates.
+
+
+$Add-AzureRmAccount 
+
+$Set-AzureRmContext -SubscriptionName  "IDC Backup Subscription - Visual Studio Enterprise"
+
+
+// Creating a new azure resource group (optional ,can use other resource groups)
+$New-AzureRmResourceGroup -Name StorageTemplateTest  -Location "East US"
+
+//Testing a template
+$Test-AzureRmResourceGroupDeployment -ResourceGroupName StorageTemplateTest -TemplateFile C:\Users\Srujan.Goregey\Desktop\Notes\json\azureStorageTemplate.json  -debug
